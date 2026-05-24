e26f60e4c4f0593c4c248ad4411d02a1f111f8db
isconaifa <iscobasdostdbv@gmail.com>
2026-05-23 22:16:01 -0400

    ajuste de documentação

--- src/main/java/br/com/clinicanaifa/config/SpringDoc.java
+++ src/main/java/br/com/clinicanaifa/config/SpringDoc.java
@@ -2,6 +2,9 @@ package br.com.clinicanaifa.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +17,18 @@ public class SpringDoc {
         return new OpenAPI()
                 .components(new Components()
                         .addSecuritySchemes("bearer-key",
-                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .info(new Info()
+                        .title("Agendamento de consulatas API")
+                        .description("API Rest da aplicação de agendamento de consulta médica, contendo as funcionalidades de CRUD de médicos e de pacientes, além de agendamento e cancelamento de consultas")
+                        .contact(new Contact()
+                                .name("Dev Backend")
+                                .email("francisco.naifa@sou.ufmt.br"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://fnaifa/api/licenca")));
     }
 }
